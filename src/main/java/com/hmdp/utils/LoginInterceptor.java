91f54ca7f370e2decb5b07a49bd64b12bbd34441
Hegra-727 <Hegra727@outlook.com>
2023-11-20 20:02:54 +0800

    拦截器拆分，判断ThreadLocal中有无对象存入

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -11,20 +11,11 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //1. 获取session
-        HttpSession session = request.getSession();
-        //2.获取session中的用户
-        Object user = session.getAttribute("user");
-        //3. 判断用户是否存在
-        if (user == null){
-            //4. 不存在，拦截
+        //判断threadLocal中有无user对象存在
+        if(UserHolder.getUser()==null){
             response.setStatus(401);
             return false;
         }
-
-        //5. 存在 保存用户信息到ThreadLocal
-        UserHolder.saveUser((UserDTO) user);
-        //6. 放行
         return true;
     }
 
